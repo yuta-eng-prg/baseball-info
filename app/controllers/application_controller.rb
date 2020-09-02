7d9b92b027343dab432a9327306a0af6ebcbc790
yuta-eng-prg <horii6773@gmail.com>
2020-09-02 18:52:07 +0900

    新規登録ページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+                                        :nickname,
+                                        :email,
+                                        :password,
+                                      ])
+  end
+
+
+  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
+
+  validates :email, uniqueness: { case_sensitive: true }, format: { with: EMAIL_REGEX }
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :nickname, presence: true, uniqueness: { case_sensitive: true }
 end
