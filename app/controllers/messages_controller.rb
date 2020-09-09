da85addebe6312398ce777ec6d0d87c1a4111919
yuta-eng-prg <horii6773@gmail.com>
2020-09-09 09:05:00 +0900

    メッセージコントローラーの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def about
     
