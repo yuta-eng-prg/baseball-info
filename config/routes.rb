64e78458f1ab292c68e5eee3b5fc89bc1c6dfc0f
yuta-eng-prg <horii6773@gmail.com>
2020-09-09 09:05:50 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'messages#about'
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :messages do
+
+    resources :helpful_messages, only: [:create, :destroy]
     resources :comments, only: [:index, :create, :edit, :update, :destroy]
   end
   
